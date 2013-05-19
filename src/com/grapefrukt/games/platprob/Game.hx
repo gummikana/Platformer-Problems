9a277bdec760140413ed0cdfcef29f87f5341fe4
grapefrukt <grapefrukt@grapefrukt.com>
2013-05-19 19:49:26 +0200

    First commit

---
+++ src/com/grapefrukt/games/platprob/Game.hx
@@ -0,0 +1,19 @@
+package com.grapefrukt.games.platprob;
+
+import nme.display.Sprite;
+
+/**
+ * ...
+ * @author Martin Jonasson, m@grapefrukt.com
+ */
+class Game extends Sprite {
+
+	public function new() {
+		super();
+	}
+	
+	public function init() {
+		
+	}
+	
+}
