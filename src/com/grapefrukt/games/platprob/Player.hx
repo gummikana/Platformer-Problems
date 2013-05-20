2cdf71f12853c13b0aad5043cdbfbb21131e4f8f
gummikana <petri.purho+github@gmail.com>
2013-05-20 10:29:06 +0200

    Added jump height trace

--- src/com/grapefrukt/games/platprob/Player.hx
+++ src/com/grapefrukt/games/platprob/Player.hx
@@ -13,8 +13,13 @@ class Player {
 	
 	public var body(default, null):B2Body;
 	public var isOnGround(default, null):Bool;
+	
+	// stats
 	public var jumpTime:Int;
 	public var jumpTimeStart:Int;
+	public var jumpHeight:Float;
+	public var jumpHighest:Float;
+	public var jumpHeightStart:Float;
 	
 	public function new(world:B2World) {
 		body = PhysUtils.createBox(world, Settings.STAGE_W / 2, Settings.STAGE_H / 4, 50, 100, true, 1);
@@ -23,22 +28,31 @@ class Player {
 		
 		isOnGround = false;
 		jumpTimeStart = 0;
+		jumpHeightStart = 0;
 	}
 	
 	public function update() {
 		isOnGround = false;
+		if ( jumpHeightStart != 0 && body.getPosition().y < jumpHighest ) jumpHighest = body.getPosition().y;
 	}
 	
 	public function jump() {
 		body.applyForce( new B2Vec2( 0, Settings.PLATFORMING_JUMP_VELOCITY ), new B2Vec2( 0, 0 ) );
+		
 		jumpTimeStart = Lib.getTimer();
+		jumpHeightStart = body.getPosition().y;
+		jumpHighest = jumpHeightStart;
 	}
 	
 	public function touchGround() {
 		if ( isOnGround == false && jumpTimeStart != 0 ) {
 			jumpTime = Lib.getTimer() - jumpTimeStart;
 			jumpTimeStart = 0;
+			jumpHeight = jumpHighest - jumpHeightStart;
+			jumpHeightStart = 0;
+			
 			Lib.trace( jumpTime );
+			Lib.trace( jumpHeight );
 		}
 		isOnGround = true;
 	}
--- src/com/grapefrukt/games/platprob/Settings.hx
+++ src/com/grapefrukt/games/platprob/Settings.hx
@@ -12,7 +12,7 @@ class Settings{
 	static public inline var PHYSICS_SCALE				:Float = 1 / 30;
 	static public inline var PHYSICS_STEP_DURATION		:Float = 1 / 60;
 	static public inline var PHYSICS_GRAVITY			:Float = 10 * 9.8;	// 10 * 9.8
-	static public inline var PLATFORMING_JUMP_VELOCITY	:Float = -10000;	// for 10x gravity -10000, -2000 for normal gravity
+	static public inline var PLATFORMING_JUMP_VELOCITY	:Float = -12500;	// for 10x gravity -10000, -2000 for normal gravity
 	static public inline var PLATFORMING_HORIZONTAL_MOVE_VELOCITY:Float = 500;
 	
 	public static function psb2(x:Float, y:Float):B2Vec2 {
