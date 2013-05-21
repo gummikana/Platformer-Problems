d759312114dcb25a8bf2bc3473937e2012df7963
gummikana <petri.purho@gmail.com>
2013-05-21 13:46:13 +0200

    Fixed ContactListener so it actually works when a contact happens

--- src/com/grapefrukt/games/platprob/physics/ContactListener.hx
+++ src/com/grapefrukt/games/platprob/physics/ContactListener.hx
@@ -4,6 +4,7 @@ import box2D.dynamics.B2ContactImpulse;
 import box2D.dynamics.B2ContactListener;
 import box2D.dynamics.contacts.B2Contact;
 import com.grapefrukt.games.platprob.Player;
+import com.grapefrukt.games.platprob.PlayerVelocity;
 import nme.Lib;
 
 /**
@@ -21,16 +22,25 @@ class ContactListener extends B2ContactListener {
 		if (!contact.isTouching()) return;
 		
 		var player:Player = null;
-		if (Type.getClass(contact.getFixtureB().getBody().getUserData()) == Player) {
+		if(Type.getClass(contact.getFixtureB().getBody().getUserData()) == Player) {
 			player = cast(contact.getFixtureB().getBody().getUserData(), Player);
-		} else if (Type.getClass(contact.getFixtureB().getBody().getUserData()) != Player) {
-			player = cast(contact.getFixtureB().getBody().getUserData(), Player);
-		} else {
-			return;
+		} else if( Type.getClass(contact.getFixtureA().getBody().getUserData()) == Player ) {
+			player = cast(contact.getFixtureA().getBody().getUserData(), Player);
 		}
 		
 		if( player != null )
 			player.touchGround();
+			
+		var player_velocity:PlayerVelocity = null;
+		if (Type.getClass(contact.getFixtureB().getBody().getUserData()) == PlayerVelocity) {
+			player_velocity = cast(contact.getFixtureB().getBody().getUserData(), PlayerVelocity);
+		} else if (Type.getClass(contact.getFixtureA().getBody().getUserData()) == PlayerVelocity) {
+			player_velocity = cast(contact.getFixtureA().getBody().getUserData(), PlayerVelocity);
+		} 
+		
+		if ( player_velocity != null ) {
+			player_velocity.onContact( contact );
+		}
 	}
 	
 	override public function postSolve(contact:B2Contact, impulse:B2ContactImpulse):Void {
