46d10d9b6bb04ab2e4c0a4ffb50f5411d3870de0
gummikana <petri.purho@gmail.com>
2013-05-22 09:17:52 +0200

    Added PLAYER_IS_A_BOX into Settings

--- src/com/grapefrukt/games/platprob/Settings.hx
+++ src/com/grapefrukt/games/platprob/Settings.hx
@@ -16,6 +16,7 @@ class Settings{
 	
 	static public var USE_VPLAYER							:Bool = false;
 	
+	
 	static public var PLATFORMING_JUMP_VELOCITY				:Float = -4375 * 1.5;	// for 10x gravity -10000, -2000 for normal gravity
 	static public var PLATFORMING_CLAMP_JUMP				:Bool = false;
 	static public var PLATFORMING_USE_IN_AIR_COUNTER		:Bool = true;
@@ -24,7 +25,9 @@ class Settings{
 	static public var PLATFORMING_HORIZONTAL_VELOCITY_ON_GROUND:Float = 250;
 	static public var PLATFORMING_HORIZONTAL_VELOCITY_IN_AIR:Float = 0.75 * 250;
 
+	static public var PLAYER_IS_A_BOX					:Bool = false;
 	static public var PLAYER_PILL_BOX_SIZE_LESS			:Float = 0.3;
+	
 	static public var PLAYER_WHEEL_STOP					:Bool = true;
 	static public var PLAYER_GROUND_SLOWDOWN			:Bool = true;
 	static public var PLAYER_GROUND_SLOWDOWN_LENGTH		:Int = 5;
